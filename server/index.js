5f9d34d86bb5271463dc548c28150c8d5d5d3485
Khen chandarapisey <piseykhenchandara93@gmail.com>
2025-06-07 12:57:29 +0700

    first commit server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require("cors");
+const app  = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req,res) => {
+    res.send('hello fromn backend brother')
+});
+app.listen(PORT, () => {
+    console.log("backend is runing on port 5000")
+})
